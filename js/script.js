b3e2a9904dcefd4920e6aae5c3106db730ec7192
Eugene Shiu <shiueugene@gmail.com>
2020-11-20 09:55:35 -0500

    add css

--- js/script.js
+++ js/script.js
@@ -11,35 +11,29 @@ function divide(x,y){
     return x/y;
   }
 
-  function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
-    if (playerSelection == "Rock"){
-      if (computerSelection == "Rock") {
-        return "Tie Game...Rock"
-        }
-      else if (computerSelection == "Paper") {
-        return "You Lose! Paper beats Rock"
-        }
-      else {
-      return "You Win! Rock beats Scissors"
-      }
-    else {
-      if (computerSelection == "Paper") {
-        return "Tie Game...Paper"
-        }
-      else if (computerSelection == "Scissors") {
-        return "You Lose! Scissors beat Paper"
-        }
-      else {
-      return "You Win! Paper beat Rock"
-      }
-    }
+function operate(op,num1,num2)
+{
+  switch(op)
+  {
+    case '+':
+      let answer = add(num1,num2)
+      console.log(answer)
+      break;
+    case '-':
+      subtract(num1,num2)
+      break;
+    case '*':
+      multiply(num1,num2)
+      break;
+    case '/':
+      divide(num1,num2)
+      break;
   }
+}
 
-  function game(){
-    for (i = 0; i< 5; i++)
-    {
-        let playerSelection = prompt("Rock, Paper, or Scissors?");
-        console.log(playRound(playerSelection, computerPlay()));
-    }
-  }
+console.log(add(5,3))
+console.log(operate('+',5,4))
+console.log(operate('-',5,2))
+console.log(operate('*',5,4))
+console.log(operate('/',5,2))
+
