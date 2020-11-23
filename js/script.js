34434554ac7ec6bd161c9b0ce65fbb9b3b2673b4
Eugene Shiu <shiueugene@gmail.com>
2020-11-23 11:17:34 -0500

    sample codes

--- js/script.js
+++ js/script.js
@@ -1,39 +1,125 @@
-function add(x,y){
-    return x+y;
-  }
-function subtract(x,y){
-    return x-y;
-  }
-function multiply(x,y){
-    return x*y;
-  }
-function divide(x,y){
-    return x/y;
-  }
+let operator = '';
+let previousNumber = '';
+let currentNumber = '';
 
-function operate(op,num1,num2)
-{
-  switch(op)
-  {
-    case '+':
-      let answer = add(num1,num2)
-      console.log(answer)
-      break;
-    case '-':
-      subtract(num1,num2)
-      break;
-    case '*':
-      multiply(num1,num2)
-      break;
-    case '/':
-      divide(num1,num2)
-      break;
+function changeSign(){
+  currentNumber = currentNumber.toString();
+  if(currentNumber && currentNumber.includes('-')) {
+    currentNumber = Math.abs(currentNumber);
+  } else {
+    currentNumber = - currentNumber  
   }
 }
 
-console.log(add(5,3))
-console.log(operate('+',5,4))
-console.log(operate('-',5,2))
-console.log(operate('*',5,4))
-console.log(operate('/',5,2))
+function clearAll() {
+  previousNumber = '';
+  currentNumber = '';
+  operator = '';
+}
 
+function clearEntry(){
+  currentNumber = '';
+}
+
+
+function appendNumber(e) {
+  if(currentNumber == Number.POSITIVE_INFINITY || currentNumber == Number.NEGATIVE_INFINITY) return
+  let number = e.target.textContent.toString();
+  if(number === '+/-') return;
+  if(number === '.' && currentNumber.includes('.')) return;
+  if(currentNumber.length === 19) return;
+  currentNumber += number;
+}
+
+function getOperator(e){
+  if(currentNumber === '' ||currentNumber == Number.POSITIVE_INFINITY ||
+    currentNumber == Number.NEGATIVE_INFINITY) return
+  if(previousNumber !== ''){
+    calculate();
+  }
+
+  operator = e.target.innerText.toString();
+  previousNumber = currentNumber;
+  currentNumber = '';
+}
+
+function calculate() {
+  if(previousNumber !== '' && currentNumber !== ''){
+    switch (operator){
+      case '+':
+        currentNumber = parseFloat(previousNumber) + parseFloat(currentNumber);
+        break;
+      case '-':
+        currentNumber = parseFloat(previousNumber) - parseFloat(currentNumber);
+        break;
+      case '*':
+        currentNumber = parseFloat(previousNumber) * parseFloat(currentNumber);
+        break;
+      case '÷':
+        currentNumber = parseFloat(previousNumber) / parseFloat(currentNumber);
+        break;
+      default:
+        return
+    }
+    previousNumber = '';
+    operator = '';
+  }
+}
+
+function updateDisplay() {
+  let $currentDisplay = document.querySelector('#display-current');
+  let $previousDisplay = document.querySelector('#display-previous');
+  
+  if (currentNumber == Number.POSITIVE_INFINITY || currentNumber == Number.NEGATIVE_INFINITY) {
+    $currentDisplay.innerText = 'Error'
+    $previousDisplay.innerText = `${previousNumber} ${operator}`;
+    return
+  };
+
+  $currentDisplay.innerText = currentNumber;
+  $previousDisplay.innerText = `${previousNumber} ${operator}`;
+}
+
+function inputManager() {
+  const $buttons = document.querySelectorAll('#buttons .button');
+  $buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      appendNumber(e);
+      updateDisplay();
+    });
+  });
+
+  const $operators = document.querySelectorAll('#operators .operator');
+  $operators.forEach((operator) => {
+    operator.addEventListener('click', (e) => {
+      getOperator(e);
+      updateDisplay();
+    });
+  });
+
+  const $clear = document.querySelector('#clear');
+  $clear.addEventListener('click',() => {
+    clearAll();
+    updateDisplay();
+  });
+
+  const $equals = document.querySelector('#equals');
+  $equals.addEventListener('click', () => {
+    calculate();
+    updateDisplay();
+  });
+
+  const $clearEntry = document.querySelector('#clearEntry');
+  $clearEntry.addEventListener('click', () => {
+    clearEntry();
+    updateDisplay();
+  });
+
+  const $plusMinus = document.querySelector('#plus-minus');
+  $plusMinus.addEventListener('click', () => {
+    changeSign();
+    updateDisplay();
+  });
+}
+
+inputManager();
